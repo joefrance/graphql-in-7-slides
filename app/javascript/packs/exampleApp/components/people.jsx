08e02d7190f112e8cc2753ae358086ab69c14eb9
Joe France <joefrance@totalwebsystems.com>
2018-12-12 09:57:23 -0500

    Add People and Posts GraphQL calls

--- app/javascript/packs/exampleApp/components/people.jsx
+++ app/javascript/packs/exampleApp/components/people.jsx
@@ -1,11 +1,53 @@
 import React from "react";
-class Pricing extends React.Component {
+const axios = require("axios");
+
+class People extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      test: 1,
+      people: []
+    };
+
+    console.log(`constructor`);
+    //this.componentDidMount = this.componentDidMount.bind(this);
+  }
+
+  componentDidMount() {
+    axios({
+      url: "http://localhost:3000/graphql",
+      method: "post",
+      data: {
+        query: `
+          query allPeople {
+            allPeople {
+              id
+              fullName
+            }
+          }
+        `
+      }
+    }).then(result => {
+      this.setState({ people: result.data.data.allPeople });
+      console.log(result.data.data.allPeople);
+      console.log(this.state.people);
+    });
+
+    console.log("People did mount.");
+  }
+
   render() {
     return (
       <div>
         <h1>People</h1>
+        <ul>
+          {this.state.people.map(p => {
+            return <li>{p.fullName}</li>;
+          })}
+        </ul>
       </div>
     );
   }
 }
-export default Pricing;
+export default People;
--- app/javascript/packs/exampleApp/components/posts.jsx
+++ app/javascript/packs/exampleApp/components/posts.jsx
@@ -1,11 +1,62 @@
 import React from "react";
-class Pitch extends React.Component {
+const axios = require("axios");
+
+class Post extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      test: 1,
+      posts: []
+    };
+
+    console.log(`constructor`);
+    //this.componentDidMount = this.componentDidMount.bind(this);
+  }
+
+  componentDidMount() {
+    axios({
+      url: "http://localhost:3000/graphql",
+      method: "post",
+      data: {
+        query: `
+          query allPosts {
+            allPosts {
+              author_id
+              headline
+              body
+              bodyWithHeadline
+              author {
+                fullName
+              }
+            }
+          }
+        `
+      }
+    }).then(result => {
+      this.setState({ posts: result.data.data.allPosts });
+      //console.log(result.data.data.allPosts);
+      console.log(this.state.posts);
+    });
+
+    console.log("Posts did mount.");
+  }
+
   render() {
     return (
       <div>
         <h1>Posts</h1>
+        <ul>
+          {this.state.posts.map(p => {
+            return (
+              <li>
+                {p.body} <b>by: {p.author.fullName}</b>
+              </li>
+            );
+          })}
+        </ul>
       </div>
     );
   }
 }
-export default Pitch;
+export default Post;
